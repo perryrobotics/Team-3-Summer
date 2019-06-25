4e519a6359c9ec225bae6847afacdde6f866c5fd
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-06-25 13:42:47 -0500

    Add files via upload

--- left.py
+++ left.py
@@ -9,7 +9,7 @@ def left_path():
 	move_back(1500, 700)
 	move_right(1000, 50)
 	right_to_white(1000)
-	move_right(1500, 100)
+	move_right(750, 150)
 	move(1500, 300)
 	top_arm(50)
 	move(1500, 1300)
@@ -17,3 +17,4 @@ def left_path():
 	move_back(1000, 3000)
 	up_arm(50)
 	move_right(1000, 250)
+	down_arm(50)
